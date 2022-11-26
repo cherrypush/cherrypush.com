4786fbf8d51b58f057558b408fa508fd6aae45da
Flavio Wuensche <f.wuensche@gmail.com>
2022-11-26 12:06:50 +0100

    clear previous occurrences after cherry push

--- app/controllers/api/occurrences_controller.rb
+++ app/controllers/api/occurrences_controller.rb
@@ -2,27 +2,21 @@
 
 class Api::OccurrencesController < Api::ApplicationController
   def create
-    JSON
-      .parse(params.require(:occurrences))
-      .map { |occurrence| Occurrence.create!(build_occurrence(occurrence)) }
+    new_occurrences = JSON.parse(params.require(:occurrences))
+    previous_occurrences_ids = Occurrence.where(repo: new_occurrences[0]['repo']).ids # TODO: rethink how to provide the repo
+    new_occurrences.map { |occurrence| Occurrence.create!(build_occurrence(occurrence)) }
+    Occurrence.where(id: previous_occurrences_ids).destroy_all
 
     render json: { status: :ok }, status: :ok
   rescue StandardError => e
-    render json: { status: :error, message: e }, status: :bad_request
+    render json: { status: :error, message: e }
+    raise e
   end
 
   private
 
   # this simulates strong params
   def build_occurrence(occurrence)
-    occurrence.slice(
-      "metric_name",
-      "commit_sha",
-      "file_path",
-      "line_number",
-      "line_content",
-      "repo",
-      "owners"
-    )
+    occurrence.slice('metric_name', 'commit_sha', 'file_path', 'line_number', 'line_content', 'repo', 'owners')
   end
 end
--- test/controllers/occurrences_controller_test.rb
+++ test/controllers/occurrences_controller_test.rb
@@ -1,19 +1,27 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class OccurrencesControllerTest < ActionDispatch::IntegrationTest
-  test "creates basic occurrences" do
+  test 'creates basic occurrences' do
     occurrences = build_list(:occurrence, 3)
     post(api_occurrences_path, params: { occurrences: occurrences.to_json })
     assert_response :ok
     assert_equal 3, Occurrence.count
   end
 
-  test "creates occurrences with owners" do
-    occurrence = build(:occurrence, owners: ["@fwuensche"])
+  test 'creates occurrences with owners' do
+    occurrence = build(:occurrence, owners: ['@fwuensche'])
     post(api_occurrences_path, params: { occurrences: [occurrence].to_json })
     assert_response :ok
-    assert_includes Occurrence.first.owners, "@fwuensche"
+    assert_includes Occurrence.first.owners, '@fwuensche'
+  end
+
+  test 'cleans previous occurrences' do
+    occurrence = build(:occurrence)
+    post(api_occurrences_path, params: { occurrences: [occurrence].to_json })
+    post(api_occurrences_path, params: { occurrences: [occurrence].to_json })
+    assert_response :ok
+    assert_equal 1, Occurrence.all.count
   end
 end
