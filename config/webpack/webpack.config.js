18d43dac6a91111a74471ec3902341f6220d8602
Flavio Wuensche <f.wuensche@gmail.com>
2022-11-26 19:22:00 +0100

    introduce-webpack (#1)

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = (env) => {
+  const isDevelopment = env.development
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: {
+      application: './app/javascript/application.js',
+    },
+    resolve: {
+      modules: ['node_modules', path.resolve('app/javascript')], // use <root>/app/javascript as a base directory for modules
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
+      chunkFilename: '[name]-[contenthash].digested.js',
+      sourceMapFilename: '[file]-[fullhash].digested.map',
+    },
+    plugins: [],
+    module: {
+      rules: [],
+    },
+  }
+}
