9e8be093b07c1ba844bc4dcb0c0d572d0fabb1f9
Flavio Wuensche <f.wuensche@gmail.com>
2022-11-26 19:38:36 +0100

    improve tests

--- test/controllers/occurrences_controller_test.rb
+++ test/controllers/occurrences_controller_test.rb
@@ -17,11 +17,15 @@ class OccurrencesControllerTest < ActionDispatch::IntegrationTest
     assert_includes Occurrence.first.owners, '@fwuensche'
   end
 
-  test 'cleans previous occurrences' do
-    occurrence = build(:occurrence)
-    post(api_occurrences_path, params: { occurrences: [occurrence].to_json })
-    post(api_occurrences_path, params: { occurrences: [occurrence].to_json })
+  test 'cleans previous occurrences of the same project' do
+    create(:occurrence, repo: 'rails/rails')
+    create(:occurrence, repo: 'ruby/ruby')
+    new_occurrence = build(:occurrence, repo: 'cherrypush/cherry-cli')
+    post(api_occurrences_path, params: { occurrences: [new_occurrence].to_json })
+    post(api_occurrences_path, params: { occurrences: [new_occurrence].to_json })
     assert_response :ok
-    assert_equal 1, Occurrence.all.count
+    assert_equal 3, Occurrence.all.count
+    assert_equal 1, Occurrence.where(repo: 'rails/rails').count
+    assert_equal 1, Occurrence.where(repo: 'ruby/ruby').count
   end
 end
