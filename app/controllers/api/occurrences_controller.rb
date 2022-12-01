7eeed6796f812339e65a722a82680299516fa5bd
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-01 09:18:12 +0100

    extract more components

--- app/controllers/api/occurrences_controller.rb
+++ app/controllers/api/occurrences_controller.rb
@@ -2,12 +2,12 @@
 
 class Api::OccurrencesController < Api::ApplicationController
   before_action :set_user
-  before_action :project, only: :create
+  before_action :set_project, only: :create
 
   # TODO: rethink the mechanism to clean up duplicates for the same repo. this is currently a mess.
   def create
     previous_occurrences_ids = @project.occurrences.ids # used for cleanup
-    new_occurrences.each { |occurrence| project.occurrences.create!(build_occurrence(occurrence)) }
+    new_occurrences.each { |occurrence| @project.occurrences.create!(build_occurrence(occurrence)) }
     Occurrence.where(id: previous_occurrences_ids).destroy_all
 
     render json: { status: :ok }, status: :ok
@@ -18,8 +18,8 @@ class Api::OccurrencesController < Api::ApplicationController
 
   private
 
-  # this simulates strong params
   def build_occurrence(occurrence)
+    # TODO: any way to make it via strong params?
     occurrence.slice('metric_name', 'commit_sha', 'file_path', 'line_number', 'line_content', 'owners')
   end
 
@@ -27,7 +27,8 @@ class Api::OccurrencesController < Api::ApplicationController
     @user = User.find_by(api_key: params[:api_key])
   end
 
-  def project
+  def set_project
+    # TODO: it seems really wrong to be using the repo value of the first occurrence to find the project
     @project = @user.projects.find_or_create_by!(name: new_occurrences[0]['repo'])
   end
 
