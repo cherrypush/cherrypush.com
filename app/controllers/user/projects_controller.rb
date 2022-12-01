e8ad4e11f4db41b05e90d0731430670ac890438b
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-01 12:00:44 +0100

    remove destroy project

--- app/controllers/user/projects_controller.rb
+++ app/controllers/user/projects_controller.rb
@@ -4,15 +4,4 @@ class User::ProjectsController < ApplicationController
   def index
     @projects = current_user.projects
   end
-
-  def destroy
-    current_user.projects.find(params[:id]).destroy
-    redirect_to user_projects_path, notice: 'Project removed'
-  end
-
-  private
-
-  def project_params
-    params.require(:project).permit(:repo_url)
-  end
 end
