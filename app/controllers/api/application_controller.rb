6103e35736c5603fba5524649598b42f854943e4
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-01 17:12:26 +0100

    improve controller

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,4 +2,13 @@
 
 class Api::ApplicationController < ApplicationController
   skip_before_action :verify_authenticity_token # TODO: implement auth using api keys
+
+  before_action :set_user
+
+  private
+
+  def set_user
+    @user = User.find_by(api_key: params[:api_key])
+    head :unauthorized if @user.nil?
+  end
 end
