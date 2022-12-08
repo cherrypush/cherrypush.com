0ec37adea0b3f49783cc2acf407da3d915c12723
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-08 16:37:42 +0100

    convert api occurrences into api reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_141820) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_153318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_141820) do
 
   create_table "occurrences", force: :cascade do |t|
     t.string "metric_name"
-    t.string "commit_sha"
     t.string "file_path"
     t.integer "line_number"
     t.string "line_content"
--- test/controllers/occurrences_controller_test.rb
+++ test/controllers/occurrences_controller_test.rb
@@ -6,38 +6,38 @@ class OccurrencesControllerTest < ActionDispatch::IntegrationTest
   let!(:user) { create(:user) }
 
   it 'creates basic occurrences' do
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: occurrences_json(3) })
+    post(api_reports_path(api_key: user.api_key), params: report_params(number_of_occurrences: 3))
     assert_response :ok
     project = Project.find_by!(name: 'rails/rails')
     assert_equal 3, project.reports.last.occurrences.count
   end
 
   it 'creates occurrences with owners' do
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: [new_occurrence].to_json })
+    post(api_reports_path(api_key: user.api_key), params: report_params)
     assert_response :ok
     assert_includes Occurrence.first.owners, '@fwuensche'
   end
 
   it 'keeps previous occurrences of the same project in a separate report' do
-    create(:project, name: 'rails/rails', user: user)
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: [new_occurrence('rails/rails')].to_json })
+    create(:project, name: 'rails/rails', user:)
+    post(api_reports_path(api_key: user.api_key), params: report_params(project_name: 'rails/rails'))
     assert_equal 1, Report.last.occurrences.count
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: [new_occurrence('rails/rails')].to_json })
+    post(api_reports_path(api_key: user.api_key), params: report_params(project_name: 'rails/rails'))
     assert_equal 1, Report.last.occurrences.count
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: [new_occurrence('ruby/ruby')].to_json })
+    post(api_reports_path(api_key: user.api_key), params: report_params(project_name: 'ruby/ruby'))
     assert_equal 3, Report.count
     assert_equal 2, Project.find_by(name: 'rails/rails').reports.count
     assert_equal 1, Project.find_by(name: 'ruby/ruby').reports.count
   end
 
   it 'creates projects as public by default for non-premium users' do
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: [new_occurrence].to_json })
+    post(api_reports_path(api_key: user.api_key), params: report_params)
     assert_equal 'public', Project.last.access
   end
 
   it 'creates projects as private by default for premium users' do
     create(:membership, user:)
-    post(api_occurrences_path(api_key: user.api_key), params: { occurrences: [new_occurrence].to_json })
+    post(api_reports_path(api_key: user.api_key), params: report_params)
     assert_equal 'private', Project.last.access
   end
 
@@ -46,7 +46,6 @@ class OccurrencesControllerTest < ActionDispatch::IntegrationTest
   def new_occurrence(repo = 'rails/rails')
     {
       metric_name: 'react_query_v1',
-      commit_sha: '123',
       file_path: 'app/controllers/occurrences_controller.rb',
       line_number: 10,
       line_content: 'class OccurrencesController < ApplicationController',
@@ -55,7 +54,7 @@ class OccurrencesControllerTest < ActionDispatch::IntegrationTest
     }
   end
 
-  def occurrences_json(number_of_occurrences)
-    number_of_occurrences.times.map { new_occurrence }.to_json
+  def report_params(number_of_occurrences: 1, project_name: 'rails/rails')
+    { project_name:, commit_sha: '123', occurrences: number_of_occurrences.times.map { new_occurrence }.to_json }
   end
 end
