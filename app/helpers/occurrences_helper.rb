4ca4d907800b1c18d5aa73839ac62d193fa308f2
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-09 09:47:37 +0100

    allow filtering by teams

--- app/helpers/occurrences_helper.rb
+++ app/helpers/occurrences_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module OccurrencesHelper
-  def metric_names_for(project)
+  def dropdown_entries_for_metrics(project, current_team)
     project
       .reports
       .last
@@ -9,6 +9,18 @@ module OccurrencesHelper
       .map(&:metric_name)
       .uniq
       .sort
-      .map { |metric_name| { title: metric_name, url: project_path(project, metric_name:) } }
+      .map { |metric_name| { title: metric_name, url: project_path(project, metric_name:, team_name: current_team) } }
+  end
+
+  def dropdown_entries_for_teams(project, current_metric)
+    project
+      .reports
+      .last
+      .occurrences
+      .map(&:owners)
+      .flatten
+      .uniq
+      .sort
+      .map { |team_name| { title: team_name, url: project_path(project, metric_name: current_metric, team_name:) } }
   end
 end
