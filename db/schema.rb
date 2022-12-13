0d158ad22dca3ccfa03585823feb0840a37572b4
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-13 16:17:30 +0100

    add commit date to reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_160459) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_151211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_160459) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "commit_date", null: false
     t.index ["project_id"], name: "index_reports_on_project_id"
   end
 
