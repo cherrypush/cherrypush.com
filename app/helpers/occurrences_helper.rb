09df6ff21a1202ab25a0f059f7051a0af213a52e
Flavio Wuensche <f.wuensche@gmail.com>
2022-12-19 17:13:55 +0100

    allow to filter by multiple teams

--- app/helpers/occurrences_helper.rb
+++ app/helpers/occurrences_helper.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 module OccurrencesHelper
-  def metric_dropdown_entries(project, owner)
+  def metric_dropdown_entries(project, owners)
     project.metrics.map do |metric|
-      { title: metric.name, url: project_path(project, metric_name: metric.name, owner_handle: owner&.handle) }
+      { title: metric.name, url: project_path(project, metric_name: metric.name, owner_handles: owners&.map(&:handle)) }
     end
   end
 
   def owner_dropdown_entries(project, metric)
     project.owners.map do |owner|
-      { title: owner.handle, url: project_path(project, metric_name: metric&.name, owner_handle: owner.handle) }
+      { title: owner.handle, url: project_path(project, metric_name: metric&.name, owner_handles: [owner.handle]) }
     end
   end
 end
