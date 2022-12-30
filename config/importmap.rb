89530ce3e70a7fdcb8d1cf187b7ddf571ed098de
Romain Choquet <romain.choquet@gmail.com>
2022-12-30 17:21:51 +0100

    add a simple search

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin 'flowbite', to: 'https://ga.jspm.io/npm:flowbite@1.5.4/dist/flowbite.js'
 pin 'chartkick', to: 'chartkick.js'
 pin 'Chart.bundle', to: 'Chart.bundle.js'
 pin 'apexcharts', to: 'https://ga.jspm.io/npm:apexcharts@3.36.3/dist/apexcharts.common.js'
+pin "@tarekraafat/autocomplete.js", to: "https://ga.jspm.io/npm:@tarekraafat/autocomplete.js@10.2.7/dist/autoComplete.min.js"
