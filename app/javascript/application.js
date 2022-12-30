5038346dd22ce00e6759aa1eb47afdaa5a152deb
Romain Choquet <romain.choquet@gmail.com>
2022-12-30 15:49:05 +0100

    apexcharts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,32 @@
 import 'flowbite'
 import 'chartkick'
 import 'Chart.bundle'
+import ApexCharts from 'apexcharts'
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    document.querySelectorAll('.charts').forEach((chartEl) => {
+      const options = {
+        chart: {
+          background: 'none',
+          type: 'line',
+          height: 400,
+          animations: { enabled: false },
+          zoom: { enabled: false },
+          toolbar: { show: false },
+        },
+        theme: { mode: 'dark', palette: 'palette2' },
+        markers: { size: 1 },
+        series: [
+          { name: chartEl.getAttribute('data-serie-name'), data: JSON.parse(chartEl.getAttribute('data-serie')) },
+        ],
+        grid: { show: false },
+        stroke: { curve: 'smooth' },
+      }
+
+      new ApexCharts(chartEl, options).render()
+    })
+  },
+  { once: true }
+)
