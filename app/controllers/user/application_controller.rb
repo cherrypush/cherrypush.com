ae860b8f7b05b5f91ec9b08af17613d46d150a4c
Romain Choquet <romain.choquet@gmail.com>
2022-12-30 17:49:37 +0100

    some escaping

--- app/controllers/user/application_controller.rb
+++ app/controllers/user/application_controller.rb
@@ -2,21 +2,11 @@
 
 class User::ApplicationController < ApplicationController
   before_action :authenticate_user!
-  before_action :build_search_items
 
   private
 
   def authenticate_user!
     alert = 'You must be logged in to access this page'
     redirect_to(projects_path, alert:) if current_user.nil?
-  end
-
-  def build_search_items
-    @search_items =
-      current_user.projects.flat_map do |project|
-        project.metrics.map do |metric|
-          { text: "#{metric.name} - #{project.name}", href: user_metrics_path(project:, metric_name: metric.name) }
-        end
-      end
   end
 end
