26d88958af65d18a0cf4b797f47b40b93244ec21
Romain Choquet <romain.choquet@gmail.com>
2022-12-30 15:49:05 +0100

    apexcharts

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin 'application', preload: true
 pin 'flowbite', to: 'https://ga.jspm.io/npm:flowbite@1.5.4/dist/flowbite.js'
 pin 'chartkick', to: 'chartkick.js'
 pin 'Chart.bundle', to: 'Chart.bundle.js'
+pin 'apexcharts', to: 'https://ga.jspm.io/npm:apexcharts@3.36.3/dist/apexcharts.common.js'
