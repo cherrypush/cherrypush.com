86dcc9367be07a607d9a0ba175da441dda4f43cc
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-07 12:21:52 +0100

    activate sentry performance

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,6 @@ if Rails.env.production?
   Sentry.init do |config|
     config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    config.traces_sample_rate = 0
+    config.traces_sample_rate = 1.0
   end
 end
