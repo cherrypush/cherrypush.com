fcf98c6e33ab42a82fda72d40bec0f74e7902d00
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-07 15:37:25 +0100

    skip rubocop

--- app/controllers/user/metrics_controller.rb
+++ app/controllers/user/metrics_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User::MetricsController < User::ApplicationController
-  def index
+  def index # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
     @project = Project.find_by(id: params[:project_id]) || current_user.reports.last&.project
     return redirect_to user_projects_path, notice: 'You first need to create a project.' if @project.nil?
     return redirect_to projects_path, notice: 'You are not authorized to view this project.' unless authorized?
@@ -17,7 +17,7 @@ class User::MetricsController < User::ApplicationController
   private
 
   # TODO: Shall we move this to a policy object?
-  def authorized?
+  def authorized? # rubocop:disable Metrics/CyclomaticComplexity
     return true if @project.nil?
     @project.public_access? || current_user&.projects&.include?(@project) ||
       current_user&.authorizations&.find_by(project: @project)
