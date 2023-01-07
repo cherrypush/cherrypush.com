a4c1cd9bf21e46befb0270f72f69db98d71082d9
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-07 13:25:39 +0100

    report all exceptions to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  Sentry.init do |config|
-    config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
-    config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    config.traces_sample_rate = 1.0
-  end
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.traces_sample_rate = 1.0
+  config.excluded_exceptions = []
 end
