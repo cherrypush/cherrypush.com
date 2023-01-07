1144586bd9dbf63c8fe8e1a42d5d1b7b2a17abd9
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-07 10:05:40 +0100

    add contributions to navbar

--- app/controllers/user/contributions_controller.rb
+++ app/controllers/user/contributions_controller.rb
@@ -2,11 +2,6 @@
 
 class User::ContributionsController < User::ApplicationController
   def index
-    @contributions = Contribution.where(project: current_user.projects)
-    @contributions =
-      @contributions
-        .where(author_name: current_user.name)
-        .or(@contributions.where(author_email: current_user.email))
-        .or(@contributions.where('author_email like ?', "%#{current_user.github_handle}%"))
+    @contributions = current_user.contributions
   end
 end
