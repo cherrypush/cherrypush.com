402e815f104de2f967767862795b466bd7f3438c
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-10 21:38:12 +0100

    fix autocomplete

--- app/javascript/components/init_autotomplete_authorizations.js
+++ app/javascript/components/init_autotomplete_authorizations.js
@@ -1,25 +1,21 @@
 import autoComplete from '@tarekraafat/autocomplete.js'
 
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    const inputEl = document.querySelector('#authorization-users-search')
+document.addEventListener('turbo:load', () => {
+  const inputEl = document.querySelector('#authorization-users-search')
 
-    if (!inputEl) return
+  if (!inputEl) return
 
-    const autoCompleteJS = new autoComplete({
-      selector: () => inputEl,
-      data: {
-        src: JSON.parse(inputEl.getAttribute('data-src')),
+  const autoCompleteJS = new autoComplete({
+    selector: () => inputEl,
+    data: {
+      src: JSON.parse(inputEl.getAttribute('data-src')),
+    },
+    resultItem: { highlight: true },
+    resultsList: { maxResults: 12 },
+    events: {
+      input: {
+        selection: (event) => (inputEl.value = event.detail.selection.value),
       },
-      resultItem: { highlight: true },
-      resultsList: { maxResults: 12 },
-      events: {
-        input: {
-          selection: (event) => (inputEl.value = event.detail.selection.value),
-        },
-      },
-    })
-  },
-  { once: true }
-)
+    },
+  })
+})
