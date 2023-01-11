589aa8530eac8924c57810c0438c5fb44c9a6720
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-11 18:13:21 +0100

    only run weekly reports on monday

--- test/tasks/mailers_test.rb
+++ test/tasks/mailers_test.rb
@@ -1,18 +1,32 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-
-class MailersTest < ActiveSupport::TestCase
+class MailersTest < ActionMailer::TestCase
   include ActionMailer::TestHelper
 
-  before { Rails.application.load_tasks }
-
   it 'delivers emails' do
-    user = create(:user)
-    project = create(:project, user: user)
-    create(:report, commit_date: Time.current, project: project)
-    create(:report, commit_date: 4.days.ago, project: project)
-    UserMailer.any_instance.expects(:weekly_report).once
-    Rake.application.invoke_task 'mailers:deliver_weekly_report'
+    travel_to '2023-01-09 11:00:00' do # monday
+      user = create(:user)
+      project = create(:project, user: user)
+      create(:report, commit_date: Time.current, project: project)
+      create(:report, commit_date: 4.days.ago, project: project)
+      assert_emails 1 do
+        # Rake.application.invoke_task 'mailers:deliver_weekly_report'
+        Rake::Task['mailers:deliver_weekly_report'].execute
+      end
+    end
+  end
+
+  it 'only delivers weekly report on mondays' do
+    travel_to '2023-01-11 11:00:00' do # wednesday
+      user = create(:user)
+      project = create(:project, user: user)
+      create(:report, commit_date: Time.current, project: project)
+      create(:report, commit_date: 4.days.ago, project: project)
+      assert_emails 0 do
+        # Rake.application.invoke_task 'mailers:deliver_weekly_report'
+        Rake::Task['mailers:deliver_weekly_report'].execute
+      end
+    end
   end
 end
