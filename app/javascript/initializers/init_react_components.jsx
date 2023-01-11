3a59a3e5fe08b8ce6a25e7f8af054d8ef54e9e44
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-12 00:28:36 +0100

    Update init_react_components.jsx

--- app/javascript/initializers/init_react_components.jsx
+++ app/javascript/initializers/init_react_components.jsx
@@ -1,11 +1,11 @@
 import React, { lazy, Suspense } from 'react'
 import * as ReactDOM from 'react-dom/client'
 
-const nodes = document.querySelectorAll('[data-component]')
-
-Array.from(nodes).forEach((node) => {
-  const props = JSON.parse(node.getAttribute('data-props'))
-  const MyComponent = lazy(() => import(`../components/${node.getAttribute('data-component')}`))
-  const root = ReactDOM.createRoot(node)
-  root.render(<MyComponent {...props} />)
+document.addEventListener('turbo:load', () => {
+  const nodes = document.querySelectorAll('[data-component]')
+  Array.from(nodes).forEach((node) => {
+    const Component = lazy(() => import(`../components/${node.getAttribute('data-component')}`))
+    const props = JSON.parse(node.getAttribute('data-props'))
+    ReactDOM.createRoot(node).render(<Component {...props} />)
+  })
 })
