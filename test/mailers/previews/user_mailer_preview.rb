2eb5bb76afa89989e363557275da2bfdb6cf29dc
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-14 14:02:28 +0100

    complete makeover of emails

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -4,4 +4,8 @@ class UserMailerPreview < ActionMailer::Preview
   def weekly_report
     UserMailer.with(user: User.first).weekly_report
   end
+
+  def welcome
+    UserMailer.with(user: User.first).welcome
+  end
 end
