5ed5ab6202fbefd88282c9a88e5385b56c5f1ca5
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-14 14:02:28 +0100

    complete makeover of emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,9 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: "Cherry Report: #{Time.current.strftime('%b %d, %Y')} 🍒")
   end
+
+  def welcome
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Welcome to Cherry 🍒')
+  end
 end
