655aabb7aeb594d77e07243b2e24000043653734
Flavio Wuensche <f.wuensche@gmail.com>
2023-01-25 00:35:09 +0100

    only init sentry in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.traces_sample_rate = 1.0
-  config.excluded_exceptions = [ActionController::RoutingError]
+if Rails.env.production?
+  Sentry.init do |config|
+    config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.traces_sample_rate = 1.0
+    config.excluded_exceptions = [ActionController::RoutingError]
+  end
 end
