7abd1be31f17e0b83218a4126338eef543a137fb
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-07 22:20:22 +0100

    fix pushes controller

--- app/controllers/api/pushes_controller.rb
+++ app/controllers/api/pushes_controller.rb
@@ -3,9 +3,38 @@
 class Api::PushesController < Api::ApplicationController
   include ProjectScoped
 
+  #   {
+  #     project_name: projectName,
+  #     report: {
+  #       commit_sha: sha,
+  #       commit_date: committedAt.toISOString(),
+  #       metrics: {
+  #         'js_loc' => {
+  #           'total' => 100,
+  #           'owners' => {
+  #             'fwuensche' => 50,
+  #           },
+  #         },
+  #       },
+  #     },
+  #     contributions:
+  #       contributions.map(
+  #         (contribution) =>
+  #           (
+  #             {
+  #               author_name: contribution.authorName,
+  #               author_email: contribution.authorEmail,
+  #               commit_sha: contribution.sha,
+  #               commit_date: contribution.date,
+  #               metrics: contribution.metrics,
+  #             }
+  #           ),
+  #       ),
+  #   }
+
   def create
     ActiveRecord::Base.transaction do
-      params[:metrics].each do |metric_name, metric_data|
+      params[:report][:metrics].each do |metric_name, metric_data|
         metric = Metric.find_or_create_by!(name: metric_name, project: current_project)
         metric.reports.create!(
           date: params[:commit_date],
