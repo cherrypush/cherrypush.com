5880219072e370d2342262812906504d5973d926
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-10 21:59:11 +0100

    add-occurrences (#16)

--- app/controllers/api/pushes_controller.rb
+++ app/controllers/api/pushes_controller.rb
@@ -3,6 +3,52 @@
 class Api::PushesController < Api::ApplicationController
   include ProjectScoped
 
+  def create
+    params[:report] ? create_deprecated : create_new
+    render json: { status: :ok }, status: :ok
+  end
+
+  private
+
+  # NEW
+  # payload = {
+  #   project_name: 'cherrypush/cherry-app',
+  #   date: '2023-02-07T21:33:15.000Z',
+  #   metrics: [
+  #     {
+  #       name: 'missing coverage',
+  #       value: 123, # (opt.) -> if not provided, then it's calculated from occurrences
+  #       value_by_owner: { # (opt.)
+  #         ditto: 12,
+  #         bear: 13,
+  #       },
+  #       occurrences: [ # (opt.) -> if not provided, then value is mandatory
+  #         { name: name, url: url },
+  #       ],
+  #     },
+  #   ],
+  # }
+
+  def create_new
+    ActiveRecord::Base.transaction do
+      params
+        .require(:metrics)
+        .each do |metric_params|
+          metric = Metric.find_or_create_by!(name: metric_params['name'], project: current_project)
+          report =
+            metric.reports.create!(
+              date: params[:date] || Time.current,
+              value: metric_params['value'] || metric_params['occurrences'].count,
+              value_by_owner: metric_params['value_by_owner'] || value_by_owner(metric_params['occurrences']),
+            )
+          metric_params['occurrences']&.each do |occurrence| # TODO: optimize to import all at once
+            report.occurrences.create!(name: occurrence['name'], url: occurrence['url'])
+          end
+        end
+    end
+  end
+
+  # PREVIOUS
   # {
   #   'project_name' => 'cherrypush/cherry-app',
   #   'report' => {
@@ -19,7 +65,7 @@ class Api::PushesController < Api::ApplicationController
   #   },
   # }
 
-  def create
+  def create_deprecated
     ActiveRecord::Base.transaction do
       params[:report][:metrics].each do |metric_name, metric_data|
         metric = Metric.find_or_create_by!(name: metric_name, project: current_project)
@@ -29,23 +75,11 @@ class Api::PushesController < Api::ApplicationController
           value_by_owner: metric_data['owners'],
         )
       end
-
-      Contribution.upsert_all(contributions, unique_by: %i[project_id commit_sha]) if contributions.present?
     end
-    render json: { status: :ok }, status: :ok
   end
 
-  private
-
-  def contributions
-    @contributions ||=
-      Array
-        .wrap(params[:contributions])
-        .map do |contribution_params|
-          contribution_params.slice(:author_name, :author_email, :commit_sha, :commit_date, :metrics).merge(
-            project_id: current_project.id,
-          )
-        end
-        .select { |contribution_params| contribution_params[:metrics].present? }
+  def value_by_owner(occurrences)
+    return nil if occurrences.empty?
+    occurrences.map { |occurrence| occurrence['owners'] }.flatten.tally
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,7 @@
 
 class Report < ApplicationRecord
   belongs_to :metric
+  has_many :occurrences, dependent: :destroy
 
   validates :date, presence: true
   validates :value, presence: true
