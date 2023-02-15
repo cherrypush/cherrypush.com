e12f505b2a2db294d33e8731ea197bae6dae44c9
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-15 14:47:31 +0100

    add default value to reports value by owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_202256) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_134436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -116,7 +116,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_202256) do
   create_table "reports", force: :cascade do |t|
     t.datetime "date", precision: nil
     t.float "value"
-    t.jsonb "value_by_owner"
+    t.jsonb "value_by_owner", default: {}
     t.bigint "metric_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
