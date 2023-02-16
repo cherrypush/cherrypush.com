87ba03ec342aea16b1cbec1022d59598be500db8
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-17 00:40:12 +0100

    eager require all test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'rails/test_help'
 
 # PROJECT SPECIFIC
 require 'minitest/mock'
+Dir[Rails.root.join('test', 'helpers', '**', '*.rb')].each { |file| require file }
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
