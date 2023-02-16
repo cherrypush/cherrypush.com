d63b83ec3a820db252de9c7e295376a1d4178f7b
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-17 00:53:59 +0100

    migrate more to react

---
+++ app/javascript/components/MetricsIndex.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import DebtOwners from './DebtOwners'
+import Occurrences from './Occurrences'
+import MetricChart from './MetricChart'
+
+const MetricsIndex = ({ selectedOwners, owners, occurrences, metricId }) => (
+  <>
+    {metricId && <MetricChart metricId={metricId} />}
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+      {owners && (
+        <div className="col-span-1">
+          <DebtOwners selectedOwners={selectedOwners} owners={owners} />
+        </div>
+      )}
+      {occurrences && (
+        <div className="col-span-1 xl:col-span-2">
+          <Occurrences occurrences={occurrences} />
+        </div>
+      )}
+    </div>
+  </>
+)
+
+export default MetricsIndex
