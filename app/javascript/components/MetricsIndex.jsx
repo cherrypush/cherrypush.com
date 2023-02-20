41d3b98baa17ffc3255a07cb3ac3aa28a08f672e
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-20 12:12:28 +0100

    cosmetic

--- app/javascript/components/MetricsIndex.jsx
+++ app/javascript/components/MetricsIndex.jsx
@@ -6,14 +6,14 @@ import MetricChart from './MetricChart'
 const MetricsIndex = ({ selectedOwners, owners, occurrences, metricId }) => (
   <>
     {metricId && <MetricChart metricId={metricId} />}
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-3">
       {owners && (
         <div className="col-span-1">
           <DebtOwners selectedOwners={selectedOwners} owners={owners} />
         </div>
       )}
       {occurrences && (
-        <div className="col-span-1 xl:col-span-2">
+        <div className="col-span-1 xl:col-span-3">
           <Occurrences occurrences={occurrences} />
         </div>
       )}
