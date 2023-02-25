255e403066a06737a07f0e261122c48b7fe2c68a
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-25 15:34:01 +0100

    fix react className

--- app/javascript/components/BackfillInstructions.jsx
+++ app/javascript/components/BackfillInstructions.jsx
@@ -1,12 +1,12 @@
 import React, { useState, Fragment } from 'react'
 
 const BackfillInstructions = () => (
-  <div class="card mb-3">
-    <p class="text-sm font-normal text-gray-500 dark:text-gray-400">
+  <div className="card mb-3">
+    <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
       Fill up your project with historic data by running the following command:
     </p>
-    <div class="prose dark:prose-invert max-w-none mb-3">
-      <pre class="mt-3">cherry backfill --api-key={window.current_user.api_key}</pre>
+    <div className="prose dark:prose-invert max-w-none mb-3">
+      <pre className="mt-3">cherry backfill --api-key={window.current_user.api_key}</pre>
     </div>
   </div>
 )
