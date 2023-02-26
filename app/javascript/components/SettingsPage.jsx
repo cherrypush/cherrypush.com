a866c6842d2dca05cd5693d29ca878b9598cc0e7
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-26 21:04:14 +0100

    delete dead code

--- app/javascript/components/SettingsPage.jsx
+++ app/javascript/components/SettingsPage.jsx
@@ -1,3 +1,4 @@
+import { Label, TextInput } from 'flowbite-react'
 import React from 'react'
 import useCurrentUser from '../hooks/useCurrentUser'
 
@@ -8,63 +9,25 @@ const SettingsPage = () => {
     <>
       <h1>Settings</h1>
 
-      <form>
-        <div className="mb-6">
-          <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Your name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={user.name}
-            required
-            disabled
-          />
-        </div>
+      <div className="mb-3 gap-1 flex flex-col">
+        <Label htmlFor="name">Name</Label>
+        <TextInput type="text" id="name" value={user.name} disabled />
+      </div>
 
-        <div className="mb-6">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Your email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={user.email}
-            required
-            disabled
-          />
-        </div>
+      <div className="mb-3 gap-1 flex flex-col">
+        <Label htmlFor="email">Email</Label>
+        <TextInput type="email" id="email" value={user.email} disabled />
+      </div>
 
-        <div className="mb-6">
-          <label htmlFor="github_handle" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            GitHub Handle
-          </label>
-          <input
-            type="text"
-            id="github_handle"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={user.github_handle}
-            required
-            disabled
-          />
-        </div>
+      <div className="mb-3 gap-1 flex flex-col">
+        <Label htmlFor="github_handle">GitHub Handle</Label>
+        <TextInput type="text" id="github_handle" value={user.github_handle} disabled />
+      </div>
 
-        <div className="mb-6">
-          <label htmlFor="api_key" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            API Key
-          </label>
-          <input
-            type="text"
-            id="api_key"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={user.api_key}
-            required
-            disabled
-          />
-        </div>
-      </form>
+      <div className="mb-3 gap-1 flex flex-col">
+        <Label htmlFor="api_key">API Key</Label>
+        <TextInput type="text" id="api_key" value={user.api_key} disabled />
+      </div>
     </>
   )
 }
