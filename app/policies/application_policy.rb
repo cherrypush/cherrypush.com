04e8bcca2595064e9afb70e2bd27eed6482ef7d4
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-26 07:28:10 +0100

    remove default pundit policies

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -8,34 +8,6 @@ class ApplicationPolicy
     @record = record
   end
 
-  def index?
-    false
-  end
-
-  def show?
-    false
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
   class Scope
     def initialize(user, scope)
       @user = user
