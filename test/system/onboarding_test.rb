752db5dc9a3862cb7dba73250f7e600b891701a8
Flavio Wuensche <f.wuensche@gmail.com>
2023-02-26 14:35:36 +0100

    fix tests

--- test/system/onboarding_test.rb
+++ test/system/onboarding_test.rb
@@ -7,21 +7,21 @@ class OnboardingTest < ApplicationSystemTestCase
     sign_in user
     assert_text 'Create your first project'
     assert_text 'Docs'
-    click_on 'Metrics'
+    find('a', text: 'Metrics').click
     assert_text 'You need to create a project first'
 
-    click_on 'Authorizations'
+    click_on 'Avatar'
+    find('li', text: 'Authorizations').click
+    assert_text 'Authorizations'
     assert_text 'You first need to create a project'
 
     project = create(:project, user: user, name: 'rails/rails')
-    refresh
-    click_on 'Metrics'
+    find('a', text: 'Metrics').click
     assert_text 'rails/rails'
     assert_text 'Fill up your project with historic data by running the following command'
 
     create(:report, metric: create(:metric, project: project, name: 'rubocop'), value: 12, date: Time.current)
-    within('#sidebar') { click_on 'Projects' }
-    refresh
+    find('a', text: 'Projects').click
     click_on 'rails/rails'
     find('tr', text: 'rubocop').click
     assert_text 'You can start using owners on your project by adding a CODEOWNERS file to your repository'
