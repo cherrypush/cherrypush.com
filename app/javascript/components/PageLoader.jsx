9788f35c83823a8313c6a05280e742d7bb09dd90
Flavio Wuensche <f.wuensche@gmail.com>
2023-03-01 14:11:50 +0100

    improve page loader

--- app/javascript/components/PageLoader.jsx
+++ app/javascript/components/PageLoader.jsx
@@ -1,13 +1,10 @@
+import { Card, Spinner } from 'flowbite-react'
 import React from 'react'
-import ContentLoader from 'react-content-loader'
 
 const PageLoader = ({ ...rest }) => (
-  <div style={{ width: '100%' }}>
-    <ContentLoader backgroundColor="#9BA3AF" foregroundColor="#1F2937" viewBox="0 0 400 200" {...rest}>
-      <rect x="0" y="0" rx="4" ry="4" width="400" height="25" />
-      <rect x="0" y="30" rx="4" ry="4" width="400" height="170" />
-    </ContentLoader>
-  </div>
+  <Card className="text-center">
+    <Spinner />
+  </Card>
 )
 
 export default PageLoader
