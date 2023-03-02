0d2a2287dc453774f8251374d804e6d636f6c915
Flavio Wuensche <f.wuensche@gmail.com>
2023-03-02 08:15:44 +0100

    optimize load time of metrics

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  belongs_to :metric
+  belongs_to :metric, touch: true
   has_many :occurrences, dependent: :destroy
 
   validates :date, presence: true
