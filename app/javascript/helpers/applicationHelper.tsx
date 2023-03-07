f9e194bd57a2f0c7cd4ef54f04dd203b83ca8206
Flavio Wuensche <f.wuensche@gmail.com>
2023-03-07 19:25:37 +0100

    add last report to projects table

--- app/javascript/helpers/applicationHelper.tsx
+++ app/javascript/helpers/applicationHelper.tsx
@@ -7,22 +7,24 @@ export const truncateStart = (text, length) => {
 }
 
 export const timeAgoInWords = (date: Date) => {
-  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000)
+  const parsedDate = new Date(date)
+
+  const seconds = Math.floor((new Date().getTime() - parsedDate.getTime()) / 1000)
   let interval = seconds / 31536000
 
-  if (interval > 1) return Math.floor(interval) + ' years ago'
+  if (interval > 1) return Math.floor(interval) + ` year${Math.floor(interval) > 1 ? 's' : ''} ago`
 
   interval = seconds / 2592000
-  if (interval > 1) return Math.floor(interval) + ' months ago'
+  if (interval > 1) return Math.floor(interval) + ` month${Math.floor(interval) > 1 ? 's' : ''} ago`
 
   interval = seconds / 86400
-  if (interval > 1) return Math.floor(interval) + ' days ago'
+  if (interval > 1) return Math.floor(interval) + ` day${Math.floor(interval) > 1 ? 's' : ''} ago`
 
   interval = seconds / 3600
-  if (interval > 1) return Math.floor(interval) + ' hours ago'
+  if (interval > 1) return Math.floor(interval) + ` hour${Math.floor(interval) > 1 ? 's' : ''} ago`
 
   interval = seconds / 60
-  if (interval > 1) return Math.floor(interval) + ' minutes ago'
+  if (interval > 1) return Math.floor(interval) + ` minute${Math.floor(interval) > 1 ? 's' : ''} ago`
 
-  return Math.floor(seconds) + ' seconds ago'
+  return Math.floor(seconds) + ` second${Math.floor(interval) > 1 ? 's' : ''} ago`
 }
