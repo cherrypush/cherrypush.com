e9e075b56f4d5c7c53e115501dd88fe47d9410a3
Flavio Wuensche <f.wuensche@gmail.com>
2023-03-07 20:26:19 +0100

    allow to sort projects table

--- app/javascript/components/ProjectsTable.jsx
+++ app/javascript/components/ProjectsTable.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 import { timeAgoInWords } from '../helpers/applicationHelper'
 import { useProjectsIndex } from '../queries/user/projects'
+import SortedTable from './SortedTable'
 
 const ProjectsTable = () => {
   const navigate = useNavigate()
@@ -11,38 +12,43 @@ const ProjectsTable = () => {
 
   if (!projects) return null
 
-  return (
-    <Table>
-      <Table.Head>
-        <Table.HeadCell>Name</Table.HeadCell>
-        <Table.HeadCell>Owner</Table.HeadCell>
-        <Table.HeadCell>Last report</Table.HeadCell>
-      </Table.Head>
-      <Table.Body>
-        {projects.map((project) => (
-          <Table.Row
-            key={project.id}
-            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cursor-pointer"
-            onClick={() => navigate(`/user/projects?project_id=${project.id}`)}
-          >
-            <Table.Cell className="text-white">{project.name}</Table.Cell>
-            <Table.Cell>{project.user.name}</Table.Cell>
-            <Table.Cell>{timeAgoInWords(project.updated_at)}</Table.Cell>
-          </Table.Row>
-        ))}
-        <Table.Row
-          onClick={() => navigate('/user/projects/new')}
-          className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cursor-pointer"
-        >
-          <Table.Cell colSpan="100%" className="text-center">
-            <div className="flex items-center justify-center">
-              <AddIcon /> {projects.length === 0 ? 'Create your first project' : 'New Project'}
-            </div>
-          </Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Owner',
+        accessor: 'user.name',
+      },
+      {
+        Header: 'Last report',
+        accessor: 'updated_at',
+        Cell: ({ row }) => timeAgoInWords(row.original.updated_at),
+      },
+    ],
+    []
   )
+
+  const handleClick = (project) => navigate(`/user/projects?project_id=${project.id}`)
+
+  const Footer = () => (
+    <Table.Row
+      onClick={() => navigate('/user/projects/new')}
+      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 cursor-pointer"
+    >
+      <Table.Cell colSpan="100%" className="text-center">
+        <div className="flex items-center justify-center">
+          <AddIcon /> {projects.length === 0 ? 'Create your first project' : 'New Project'}
+        </div>
+      </Table.Cell>
+    </Table.Row>
+  )
+
+  const data = React.useMemo(() => projects, [projects])
+
+  return <SortedTable data={data} columns={columns} onRowClick={handleClick} Footer={Footer} />
 }
 
 export default ProjectsTable
