baf51d2288b92a22758a6468ca62cdf3160bca4b
Flavio Wuensche <f.wuensche@gmail.com>
2023-04-12 18:47:17 +0200

    add chart for percentage areas

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -4,5 +4,10 @@ class Chart < ApplicationRecord
   belongs_to :dashboard
   has_many :chart_metrics, dependent: :destroy
   validates :name, presence: true
-  enum kind: { line: 'line', area: 'area', stacked_area: 'stacked_area' }
+  enum kind: {
+         line: 'line',
+         area: 'area',
+         stacked_area: 'stacked_area',
+         stacked_percentage_area: 'stacked_percentage_area',
+       }
 end
