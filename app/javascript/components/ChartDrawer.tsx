f58b8ce9a2977c515ddbe2158b37bd2404028644
Romain Choquet <romain.choquet@gmail.com>
2023-04-15 15:30:42 +0200

    extract in separate component to properly un-mount form

--- app/javascript/components/ChartDrawer.tsx
+++ app/javascript/components/ChartDrawer.tsx
@@ -1,12 +1,90 @@
 import { Autocomplete, FormControl, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material'
 import Drawer from '@mui/material/Drawer'
 import { Button } from 'flowbite-react'
-import React, { useEffect, useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import { ChartKind, useChartsCreate, useChartsUpdate } from '../queries/user/charts'
-import { DashboardType } from '../queries/user/dashboards'
-import { useMetricsIndex } from '../queries/user/metrics'
+import { ChartType, DashboardType } from '../queries/user/dashboards'
+import { Metric, useMetricsIndex } from '../queries/user/metrics'
 import MetricChart from './MetricChart'
+
+interface ChartFormProps {
+  metrics: Metric[]
+  chart?: ChartType
+  dashboard: DashboardType
+  onClose: () => void
+}
+
+const ChartForm = ({ metrics, chart, dashboard, onClose }: ChartFormProps) => {
+  const { mutate: createChart } = useChartsCreate()
+  const { mutate: updateChart } = useChartsUpdate()
+  const [name, setName] = useState(chart?.name || '')
+  const [kind, setKind] = useState<ChartKind>(chart?.kind || ChartKind.Area)
+  const [metricIds, setMetricIds] = useState<number[]>(
+    chart?.chart_metrics.map((chartMetric) => chartMetric.metric_id) || []
+  )
+
+  const metricOptions = metrics.map((metric) => ({ id: metric.id, label: metric.name }))
+  const id = chart?.id
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        const params = { dashboard_id: dashboard.id, name, kind, metric_ids: metricIds }
+        id ? updateChart({ id, ...params }) : createChart(params)
+        onClose()
+      }}
+    >
+      <h1>{id ? 'Edit Chart' : 'New Chart'}</h1>
+      <Stack spacing={3}>
+        <FormControl fullWidth>
+          <TextField
+            fullWidth
+            autoFocus
+            label="Title"
+            value={name}
+            onChange={(event) => setName(event?.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <Autocomplete
+            multiple
+            value={metricIds.map((metricId) => metricOptions.find((metric) => metric.id === metricId))}
+            options={metricOptions}
+            renderInput={(params) => <TextField {...params} label="Metrics" />}
+            onChange={(_event, items) => setMetricIds(items.flatMap((item) => (item ? item.id : [])))}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="chart-kind-label">Chart Type</InputLabel>
+          <Select
+            name="kind"
+            labelId="chart-kind-label"
+            value={kind}
+            defaultValue={kind}
+            label="Chart Type"
+            fullWidth
+            onChange={(event) => setKind(event.target.value)}
+          >
+            <MenuItem value={ChartKind.Line}>Line</MenuItem>
+            <MenuItem value={ChartKind.Area}>Area</MenuItem>
+            <MenuItem value={ChartKind.StackedArea} disabled={metricIds.length < 2}>
+              Stacked Area
+            </MenuItem>
+            <MenuItem value={ChartKind.StackedPercentageArea} disabled={metricIds.length < 2}>
+              Stacked Percentage Area
+            </MenuItem>
+          </Select>
+        </FormControl>
+        <MetricChart kind={kind} metricIds={metricIds} />
+        <Button type="submit" disabled={!name || !kind || metricIds.length === 0}>
+          {id ? 'Update' : 'Create'}
+        </Button>
+      </Stack>
+    </form>
+  )
+}
 
 interface Props {
   open: boolean
@@ -16,93 +94,15 @@ interface Props {
 
 const ChartDrawer = ({ onClose, dashboard, open }: Props) => {
   const { chartId } = useParams()
-  const location = useLocation()
-
-  const isNewChart = !chartId
-  const currentChart = chartId ? dashboard.charts.find((chart) => chart.id === parseInt(chartId)) : undefined
-
-  const [name, setName] = useState('')
-  const [kind, setKind] = useState<ChartKind>(ChartKind.Area)
-  const [metricIds, setMetricIds] = useState<number[]>([])
-
   const { data: metrics } = useMetricsIndex({ projectId: dashboard.project_id })
-  const { mutate: createChart } = useChartsCreate()
-  const { mutate: updateChart } = useChartsUpdate()
-
-  useEffect(() => {
-    if (!chartId) {
-      setName('')
-      setKind(ChartKind.Area)
-      setMetricIds([])
-    } else {
-      setName(currentChart?.name || '')
-      setKind(currentChart?.kind || ChartKind.Area)
-      setMetricIds(currentChart?.chart_metrics.map((chartMetric) => chartMetric.metric_id) || [])
-    }
-  }, [location.pathname])
 
   if (!metrics) return null
 
-  const metricOptions = metrics.map((metric) => ({ id: metric.id, label: metric.name }))
+  const chart = chartId ? dashboard.charts.find((chart) => chart.id === parseInt(chartId)) : undefined
 
   return (
     <Drawer anchor="right" open={open} onClose={onClose} SlideProps={{ className: 'w-11/12 p-12' }}>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault()
-          isNewChart
-            ? createChart({ dashboard_id: dashboard.id, name, kind, metric_ids: metricIds })
-            : updateChart({ id: parseInt(chartId), dashboard_id: dashboard.id, name, kind, metric_ids: metricIds })
-          onClose()
-        }}
-      >
-        <h1>{isNewChart ? 'New Chart' : 'Edit Chart'}</h1>
-        <Stack spacing={3}>
-          <FormControl fullWidth>
-            <TextField
-              fullWidth
-              autoFocus
-              label="Title"
-              value={name}
-              onChange={(event) => setName(event?.target.value)}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <Autocomplete
-              multiple
-              value={metricIds.map((metricId) => metricOptions.find((metric) => metric.id === metricId))}
-              options={metricOptions}
-              renderInput={(params) => <TextField {...params} label="Metrics" />}
-              onChange={(_event, items) => setMetricIds(items.flatMap((item) => (item ? item.id : [])))}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel id="chart-kind-label">Chart Type</InputLabel>
-            <Select
-              name="kind"
-              labelId="chart-kind-label"
-              value={kind}
-              defaultValue={kind}
-              label="Chart Type"
-              fullWidth
-              onChange={(event) => setKind(event.target.value)}
-            >
-              <MenuItem value={ChartKind.Line}>Line</MenuItem>
-              <MenuItem value={ChartKind.Area}>Area</MenuItem>
-              <MenuItem value={ChartKind.StackedArea} disabled={metricIds.length < 2}>
-                Stacked Area
-              </MenuItem>
-              <MenuItem value={ChartKind.StackedPercentageArea} disabled={metricIds.length < 2}>
-                Stacked Percentage Area
-              </MenuItem>
-            </Select>
-          </FormControl>
-          <MetricChart kind={kind} metricIds={metricIds} />
-          <Button type="submit" disabled={!name || !kind || metricIds.length === 0}>
-            {isNewChart ? 'Create' : 'Update'}
-          </Button>
-        </Stack>
-      </form>
+      <ChartForm metrics={metrics} chart={chart} dashboard={dashboard} onClose={onClose} />
     </Drawer>
   )
 }
