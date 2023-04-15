33112bc18cc812798f63d7ff37a2574b83d2d7bb
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-04-15 15:33:32 +0200

    fix: deprecated capabilities on capybara driver

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
@@ -6,12 +8,12 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   Capybara.register_driver :headless_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless window-size=1400,1000])
-    Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: options)
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
   end
 
   Capybara.register_driver(:chrome) do |app|
     options = Selenium::WebDriver::Chrome::Options.new(args: %w[window-size=1400,1000])
-    Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: options)
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
   end
 
   ENV['HEADLESS'] ? driven_by(:headless_chrome) : driven_by(:chrome)
