0e61f625782197bb4ab2e05ecd2270cdd9ecd555
Flavio Wuensche <f.wuensche@gmail.com>
2023-04-26 10:52:50 +0200

    add-endpoint-for-contributions (#32)

---
+++ app/controllers/api/contributions_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Api::ContributionsController < Api::ApplicationController
+  include Api::ProjectScoped
+
+  def create
+    ActiveRecord::Base.transaction do
+      params
+        .require(:contributions)
+        .each do |contribution|
+          metric = Metric.find_or_create_by!(name: contribution.require('metric_name'), project: current_project)
+
+          metric.contributions.create!(
+            author_name: params[:author_name],
+            author_email: params[:author_email],
+            commit_sha: params[:commit_sha],
+            commit_date: params[:commit_date],
+            diff: contribution.require('diff'),
+          )
+        end
+    end
+
+    render json: { status: :ok }, status: :ok
+  end
+end
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 class Contribution < ApplicationRecord
-  belongs_to :project
+  belongs_to :metric
 
   validates :commit_sha, presence: true
   validates :commit_date, presence: true
   validates :author_name, presence: true
   validates :author_email, presence: true
-  validates :metrics, presence: true
-
-  def self.aggregate(metrics_list)
-    metrics_list.each_with_object({}) do |metrics, result|
-      metrics.each do |name, value|
-        result[name] ||= { addition: 0, deletion: 0 }
-        kind = value.positive? ? :addition : :deletion
-        result[name][kind] += value
-      end
-    end
-  end
+  validates :diff, presence: true
+  validates :diff, numericality: { only_integer: true }
 end
