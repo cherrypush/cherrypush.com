13223b9756f89750181eeda3ea4f1ce3a384d802
Flavio Wuensche <f.wuensche@gmail.com>
2023-04-26 11:21:16 +0200

    replace-owners-table-with-selector (#33)

--- app/controllers/user/owners_controller.rb
+++ app/controllers/user/owners_controller.rb
@@ -2,6 +2,15 @@
 
 class User::OwnersController < User::ApplicationController
   def index
-    render json: current_user.owners
+    if params[:metric_id]
+      metric = Metric.find(params[:metric_id])
+      authorize metric.project, :read?
+      render json: metric.owners
+    elsif params[:project_id]
+      project = authorize Project.find(params[:project_id]), :read?
+      render json: project.owners
+    else
+      render json: current_user.owners
+    end
   end
 end
