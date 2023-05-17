49b1df270d8bfb1af2eede68624a8099244e0ad3
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-05-17 16:49:22 +0200

    send emails for authorization requests

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -8,4 +8,9 @@ class UserMailerPreview < ActionMailer::Preview
   def welcome
     UserMailer.with(user: User.first).welcome
   end
+
+  def new_authorization_request
+    authorization_request = AuthorizationRequest.find_or_create_by!(user: User.first, project: Project.first)
+    UserMailer.with(user: User.first, authorization_request: authorization_request).new_authorization_request
+  end
 end
