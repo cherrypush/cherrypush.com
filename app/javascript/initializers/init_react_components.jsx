8f32f8de49a149887fb7408185e17a9fcf3ff45a
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-05-22 12:20:06 +0200

    add api key directly on docs page

--- app/javascript/initializers/init_react_components.jsx
+++ app/javascript/initializers/init_react_components.jsx
@@ -1,9 +1,8 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React, { lazy } from 'react'
 import * as ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { toast } from 'react-hot-toast'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +15,7 @@ const queryClient = new QueryClient({
 document.addEventListener('turbo:load', () => {
   const nodes = document.querySelectorAll('[data-component]')
   Array.from(nodes).forEach((node) => {
-    const Component = lazy(() => import(`../components/${node.getAttribute('data-component')}.jsx`))
+    const Component = lazy(() => import(`../components/${node.getAttribute('data-component')}.tsx`))
     const props = JSON.parse(node.getAttribute('data-props'))
     ReactDOM.createRoot(node).render(
       <BrowserRouter>
