b3308a0c054637c700384ba484a55b6728c8d387
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-05-22 12:20:06 +0200

    add api key directly on docs page

--- app/javascript/hooks/useCurrentUser.ts
+++ app/javascript/hooks/useCurrentUser.ts
@@ -3,6 +3,7 @@ import { useUsersIndex } from '../queries/user/users'
 const useCurrentUser = () => {
   const { data: users } = useUsersIndex()
 
+  if (!window.current_user) return { user: null }
   if (!users) return { user: window.current_user }
 
   return { user: users.find((user) => user.id === window.current_user.id) }
