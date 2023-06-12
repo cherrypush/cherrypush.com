fe2298501307709653d2efa28cc7a0641973736e
Flavio Wuensche <f.wuensche@gmail.com>
2023-06-12 19:43:15 +0200

    add endpoint to fetch latest metric value

---
+++ app/controllers/api/metrics_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Api::MetricsController < Api::ApplicationController
+  include Api::ProjectScoped
+
+  def show
+    project = @user.projects.find_by(name: params[:project_name])
+    return head :not_found if project.nil?
+
+    metric = project.metrics.find_by(name: params[:metric_name])
+    return head :not_found if metric.nil?
+
+    render json: { value: metric.value }
+  end
+end
