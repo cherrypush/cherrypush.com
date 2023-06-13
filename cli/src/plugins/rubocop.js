a6cc03c8351fd8f3fd6c447fe19a9721ab0ef9c1
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-06-13 12:05:57 +0200

    use bundler

--- cli/src/plugins/rubocop.js
+++ cli/src/plugins/rubocop.js
@@ -2,7 +2,7 @@ import { panic } from '../error.js'
 import sh from '../sh.js'
 
 const run = async () => {
-  const out = await sh('rubocop --format=json', { throwOnError: false })
+  const out = await sh('bundle exec rubocop --format=json', { throwOnError: false })
   let report
   try {
     report = JSON.parse(out)
