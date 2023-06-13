4079d15672dd84431f5061fac67cfc41d70fa5a1
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-06-13 15:45:41 +0200

    fix: cannot delete metrics with charts

--- test/models/metric_test.rb
+++ test/models/metric_test.rb
@@ -12,4 +12,17 @@ class ProjectTest < ActiveSupport::TestCase
       assert_equal Time.current.to_date, project.reload.updated_at.to_date
     end
   end
+
+  describe 'when chart metrics are present' do
+    let!(:metric) { create(:metric) }
+    let!(:chart_metric) { create(:chart_metric, metric: metric) }
+
+    it 'can still delete the metric' do
+      assert_equal 1, ChartMetric.count
+      assert_equal 1, Metric.count
+      metric.destroy!
+      assert_equal 0, ChartMetric.count
+      assert_equal 0, Metric.count
+    end
+  end
 end
