5b6b7dc1794c89ddf7fa14f0635b47de14adea50
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-06-14 09:20:14 +0200

    improve database clean up strategy to rely on last report per metric

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,10 +18,4 @@ class Project < ApplicationRecord
   def users
     User.where(id: authorizations.pluck(:user_id) + [user_id].uniq)
   end
-
-  def delete_old_occurrences!
-    most_recent_report = reports.order(date: :desc).first
-    old_reports = reports.where('date < ?', 24.hours.ago).where.not(id: most_recent_report.id)
-    Occurrence.where(report: old_reports).delete_all
-  end
 end
