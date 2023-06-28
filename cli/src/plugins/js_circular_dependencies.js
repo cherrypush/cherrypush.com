948cc85b4c104ee79fe15c081fe4a53384d34369
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-06-28 10:50:15 +0200

    allow to specific tsconfig on circular dependency plugin

--- cli/src/plugins/js_circular_dependencies.js
+++ cli/src/plugins/js_circular_dependencies.js
@@ -1,9 +1,11 @@
 import glob from 'glob'
 import madge from 'madge'
 
-const run = async ({ include }) => {
+const run = async ({ include, tsConfig }) => {
   const paths = glob.sync(include)
-  const madgeResult = await madge(paths)
+
+  const madgeConfig = { tsConfig } // https://github.com/pahen/madge#configuration
+  const madgeResult = await madge(paths, madgeConfig)
   const dependencies = madgeResult.circular()
 
   return dependencies.map((files) => ({
