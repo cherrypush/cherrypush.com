ea835587d24f470c0b45974e69e94e40939b3f19
Flavio Wuensche <f.wuensche@gmail.com>
2023-07-07 21:56:23 +0200

    more-generic-env-setup (#54)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.smtp_settings = {
-  user_name: 'f.wuensche@gmail.com',
-  password: Rails.application.credentials.dig(:brevo, :api_key),
-  address: 'smtp-relay.sendinblue.com',
-  port: 587,
-  domain: 'cherrypush.com',
-  authentication: :plain,
-  enable_starttls_auto: true,
-}
+if Rails.application.config.action_mailer.perform_deliveries
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'f.wuensche@gmail.com',
+    password: ENV.fetch('BREVO_API_KEY'),
+    address: 'smtp-relay.sendinblue.com',
+    port: 587,
+    domain: 'cherrypush.com',
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
+end
