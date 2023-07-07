4500e7f5d1aac9c801030f12db14b45dfe4f25f7
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-07 21:23:54 +0200

    handle missing github env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV.fetch('GITHUB_CLIENT_ID'), ENV.fetch('GITHUB_CLIENT_SECRET'), scope: 'user:email,read:org'
+  provider :github,
+           ENV.fetch('GITHUB_CLIENT_ID', nil),
+           ENV.fetch('GITHUB_CLIENT_SECRET', nil),
+           scope: 'user:email,read:org'
 end
