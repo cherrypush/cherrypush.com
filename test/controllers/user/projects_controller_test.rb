148342d39627b6ce7d160ad94bc570664b7b58a5
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-18 13:52:44 +0200

    redirect back on error

---
+++ test/controllers/user/projects_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class User::AuthorizationRequestsControllerTest < ApplicationIntegrationTest
+  let!(:user) { create(:user, name: 'Flavio Wuensche') }
+  let!(:project) { create(:project, user: user) }
+
+  describe '#index' do
+    it 'redirects unauthenticated users to home page' do
+      get(user_projects_path)
+      assert_redirected_to root_path
+    end
+
+    it 'renders the page' do
+      sign_in(user, controller_test: true)
+      get(user_projects_path)
+      assert_response :ok
+    end
+  end
+end
