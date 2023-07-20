d59de99fb3f8bd507eba398abbbb682ea2436bf0
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-20 17:54:18 +0200

    accept empty array of metrics

--- app/controllers/api/pushes_controller.rb
+++ app/controllers/api/pushes_controller.rb
@@ -5,26 +5,24 @@ class Api::PushesController < Api::ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      params
-        .require(:metrics)
-        .each do |metric_params|
-          metric = Metric.find_or_create_by!(name: metric_params.require('name'), project: current_project)
-          occurrences = metric_params[:occurrences]
-          report =
-            metric.reports.create!(
-              date: params[:date] || Time.current,
-              value: metric_params[:value] || get_value(occurrences),
-              value_by_owner: metric_params[:value_by_owner] || get_value_by_owner(occurrences),
-            )
-
-          next if occurrences.blank?
-          Occurrence.upsert_all(
-            occurrences.map do |occurrence|
-              text = occurrence['text'] || occurrence['name'] # TODO: remove name once migrated all its usage
-              occurrence.slice(:url, :value, :owners).merge(text:, report_id: report.id)
-            end,
+      params[:metrics].each do |metric_params|
+        metric = Metric.find_or_create_by!(name: metric_params.require('name'), project: current_project)
+        occurrences = metric_params[:occurrences]
+        report =
+          metric.reports.create!(
+            date: params[:date] || Time.current,
+            value: metric_params[:value] || get_value(occurrences),
+            value_by_owner: metric_params[:value_by_owner] || get_value_by_owner(occurrences),
           )
-        end
+
+        next if occurrences.blank?
+        Occurrence.upsert_all(
+          occurrences.map do |occurrence|
+            text = occurrence['text'] || occurrence['name'] # TODO: remove name once migrated all its usage
+            occurrence.slice(:url, :value, :owners).merge(text:, report_id: report.id)
+          end,
+        )
+      end
     end
 
     render json: { status: :ok }, status: :ok
