966457ddd7e8546e639baf87cea9552aa66c68d5
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-21 11:25:54 +0200

    stop refething queries on every mount and migrate to TS

--- app/javascript/initializers/init_react_components.tsx
+++ app/javascript/initializers/init_react_components.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import React, { lazy } from 'react'
-import * as ReactDOM from 'react-dom/client'
+import { lazy } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 const queryClient = new QueryClient({
@@ -9,6 +9,7 @@ const queryClient = new QueryClient({
     queries: {
       refetchOnWindowFocus: false,
       retry: false,
+      refetchOnMount: false,
     },
   },
 })
@@ -17,8 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const nodes = document.querySelectorAll('[data-component]')
   Array.from(nodes).forEach((node) => {
     const Component = lazy(() => import(`../components/${node.getAttribute('data-component')}.tsx`))
-    const props = JSON.parse(node.getAttribute('data-props'))
-    ReactDOM.createRoot(node).render(
+    const dataProps = node.getAttribute('data-props')
+    const props = dataProps ? JSON.parse(dataProps) : {}
+    createRoot(node).render(
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools initialIsOpen={false} />
