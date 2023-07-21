4efabc91598b32a573c7262c437a0c3cf03625aa
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-21 15:48:51 +0200

    improve command palette

--- app/javascript/components/CommandPalette.tsx
+++ app/javascript/components/CommandPalette.tsx
@@ -1,16 +1,17 @@
-import SearchIcon from '@mui/icons-material/Search'
 import { Button } from 'flowbite-react'
 import 'ninja-keys'
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
+import { MdSearch } from 'react-icons/md'
 import { useNavigate } from 'react-router'
 import { useDashboardsIndex } from '../queries/user/dashboards'
 import { useMetricsIndex } from '../queries/user/metrics'
 import { useProjectsIndex } from '../queries/user/projects'
+import './CommandPalette.css'
 
 export const CommandPaletteButton = () => (
   <Button pill color="dark" className="ml-3" size="xs" onClick={openCommandPalette}>
     <div className="flex items-center gap-2">
-      <SearchIcon fontSize="md" />
+      <MdSearch className="w-4 h-4" />
       <kbd className="px-1.5 py-1 text-xs font-semibold bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
         <span className="mr-1">⌘</span>
         <span>k</span>
@@ -37,42 +38,48 @@ const CommandPalette = () => {
 
   if (dashboards) {
     hotkeys = hotkeys.concat(
-      dashboards.map((dashboard) => ({
-        id: `dashboard-${dashboard.id}`,
-        title: dashboard.name,
-        section: 'Dashboards',
-        handler: () => navigate(`/user/dashboards/${dashboard.id}`),
-      }))
+      dashboards
+        .map((dashboard) => ({
+          id: `dashboard-${dashboard.id}`,
+          title: `${dashboard.name} (${dashboard.project.name})`,
+          section: 'Dashboards',
+          handler: () => navigate(`/user/dashboards/${dashboard.id}`),
+        }))
+        .sort((a, b) => a.title.localeCompare(b.title))
     )
   }
 
   if (projects) {
     hotkeys = hotkeys.concat(
-      projects.map((project) => ({
-        id: `project-${project.id}`,
-        title: project.name,
-        section: 'Projects',
-        handler: () => navigate(`/user/projects?project_id=${project.id}`),
-      }))
+      projects
+        .map((project) => ({
+          id: `project-${project.id}`,
+          title: `${project.name}`,
+          section: 'Projects',
+          handler: () => navigate(`/user/projects?project_id=${project.id}`),
+        }))
+        .sort((a, b) => a.title.localeCompare(b.title))
     )
   }
 
   if (metrics) {
     hotkeys = hotkeys.concat(
-      metrics.map((metric) => ({
-        id: `metric-${metric.id}`,
-        title: metric.name,
-        section: `Metrics for ${metric.project.name}`,
-        handler: () => navigate(`/user/projects?project_id=${metric.project_id}&metric_id=${metric.id}`),
-      }))
+      metrics
+        .map((metric) => ({
+          id: `metric-${metric.id}`,
+          title: `${metric.name} (${metric.project.name})`,
+          section: `Metrics`,
+          handler: () => navigate(`/user/projects?project_id=${metric.project_id}&metric_id=${metric.id}`),
+        }))
+        .sort((a, b) => a.title.localeCompare(b.title))
     )
   }
 
   useEffect(() => {
     if (ninjaKeys.current) ninjaKeys.current.data = hotkeys
-  }, [projects, metrics])
+  }, [projects, metrics, dashboards])
 
-  return <ninja-keys placeholder="Search for projects and metrics" hideBreadcrumbs ref={ninjaKeys} />
+  return <ninja-keys placeholder="Search for projects and metrics" hideBreadcrumbs ref={ninjaKeys} class="dark" />
 }
 
 export default CommandPalette
