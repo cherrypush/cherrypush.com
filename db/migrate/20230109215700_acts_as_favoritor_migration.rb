c7fa47adff5d25db4eddccc6810ea37683a3c39c
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-25 21:37:01 +0200

    remove dependency on migration

--- db/migrate/20230109215700_acts_as_favoritor_migration.rb
+++ db/migrate/20230109215700_acts_as_favoritor_migration.rb
@@ -5,23 +5,17 @@ class ActsAsFavoritorMigration < ActiveRecord::Migration[7.0]
     create_table :favorites, force: true do |t|
       t.references :favoritable, polymorphic: true, null: false
       t.references :favoritor, polymorphic: true, null: false
-      t.string :scope, default: ActsAsFavoritor.configuration.default_scope,
-                       null: false,
-                       index: true
+      t.string :scope, default: '', null: false, index: true
       t.boolean :blocked, default: false, null: false, index: true
       t.timestamps
     end
 
+    add_index :favorites, %w[favoritor_id favoritor_type], name: 'fk_favorites'
+    add_index :favorites, %w[favoritable_id favoritable_type], name: 'fk_favoritables'
     add_index :favorites,
-              ['favoritor_id', 'favoritor_type'],
-              name: 'fk_favorites'
-    add_index :favorites,
-              ['favoritable_id', 'favoritable_type'],
-              name: 'fk_favoritables'
-    add_index :favorites,
-              ['favoritable_type', 'favoritable_id', 'favoritor_type',
-              'favoritor_id', 'scope'],
-              name: 'uniq_favorites__and_favoritables', unique: true
+              %w[favoritable_type favoritable_id favoritor_type favoritor_id scope],
+              name: 'uniq_favorites__and_favoritables',
+              unique: true
   end
 
   def self.down
