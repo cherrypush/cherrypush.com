c9d63c41bd1d078e33c4e04c924a540cd57fcb7d
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-07-25 21:41:39 +0200

    remove rails admin test

--- test/system/admin_test.rb
+++ test/system/admin_test.rb
@@ -4,28 +4,18 @@ class AdminTest < ApplicationSystemTestCase
   let!(:user) { create(:user, name: 'Flavio Wuensche', email: 'f.wuensche@gmail.com', github_handle: 'github_handle') }
 
   it 'blocks non authenticated users' do
-    visit '/admin'
-    assert_current_path '/'
-
     visit '/blazer'
     assert_current_path '/'
   end
 
   it 'block non admin users' do
-    sign_in(user, to: '/admin')
-    assert_current_path '/user/projects'
-
-    visit '/blazer'
+    sign_in(user, to: '/blazer')
     assert_current_path '/user/projects'
   end
 
   it 'allows admin users' do
     User.stub_const(:ADMIN_GITHUB_HANDLES, ['github_handle']) do
-      sign_in(user, to: '/admin')
-      assert_current_path '/admin'
-      assert_text 'Site Administration'
-
-      visit '/blazer'
+      sign_in(user, to: '/blazer')
       assert_current_path '/blazer'
       assert_text 'New Query'
     end
