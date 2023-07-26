d013db0574265e99dd18abe9e4d183f681d9efec
Flavio Wuensche <f.wuensche@gmail.com>
2023-07-26 18:27:11 +0200

    remove n+1 from contributions controller

--- app/controllers/api/contributions_controller.rb
+++ app/controllers/api/contributions_controller.rb
@@ -5,10 +5,14 @@ class Api::ContributionsController < Api::ApplicationController
 
   def create # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     ActiveRecord::Base.transaction do
+      all_metric_names = params[:contributions].map { |c| c.require('metric_name') }
+      metrics = Metric.where(name: all_metric_names, project: current_project)
+
       params
         .require(:contributions)
         .each do |contribution_params|
-          metric = Metric.find_or_create_by!(name: contribution_params.require('metric_name'), project: current_project)
+          metric = metrics.find { |m| m.name == contribution_params.require('metric_name') }
+          next if metric.nil? # if metric is not found, we ignore the contribution
 
           contribution = metric.contributions.find_or_initialize_by(commit_sha: params[:commit_sha])
 
