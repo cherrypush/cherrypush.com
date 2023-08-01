650565342c59589ffbcc3c54830bb09323c06cfc
Flavio Wuensche <f.wuensche@gmail.com>
2023-08-02 01:46:04 +0200

    cosmetic changes

--- app/javascript/components/Footer.tsx
+++ app/javascript/components/Footer.tsx
@@ -1,3 +1,6 @@
+import { FaGithub, FaTwitter } from 'react-icons/fa'
+import { SiNpm } from 'react-icons/si'
+
 const Footer = () => (
   <>
     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
@@ -20,113 +23,29 @@ const Footer = () => (
       </ul>
       <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
         <a
-          href="https://npmjs.com/package/cherrypush"
+          href="https://twitter.com/fwuensche"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
         >
-          <svg
-            className="w-5 h-5"
-            fill="currentColor"
-            role="img"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>npm</title>
-            <path d="M1.763 0C.786 0 0 .786 0 1.763v20.474C0 23.214.786 24 1.763 24h20.474c.977 0 1.763-.786 1.763-1.763V1.763C24 .786 23.214 0 22.237 0zM5.13 5.323l13.837.019-.009 13.836h-3.464l.01-10.382h-3.456L12.04 19.17H5.113z" />
-          </svg>
+          <FaTwitter className="w-5 h-5" />
         </a>
         <a
-          href="https://github.com/cherrypush/cherry"
+          href="https://npmjs.com/package/cherrypush"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
         >
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path
-              fillRule="evenodd"
-              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <SiNpm className="w-5 h-5" />
         </a>
-      </div>
-    </footer>
-
-    {/* <!-- Main modal --> */}
-    <footer
-      id="contact-modal"
-      tabIndex="-1"
-      aria-hidden="true"
-      className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
-    >
-      <div className="relative w-full h-full max-w-md md:h-auto">
-        {/* <!-- Modal content --> */}
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <button
-            type="button"
-            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-            data-modal-toggle="contact-modal"
-          >
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <span className="sr-only">Close modal</span>
-          </button>
-          {/* <!-- Modal header --> */}
-          <div className="px-6 py-4 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-base font-semibold text-gray-900 lg:text-xl dark:text-white">Reach out to us</h3>
-          </div>
-          {/* <!-- Modal body --> */}
-          <div className="p-6">
-            <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
-              We're currently running paid plans for beta users only. If you're working on open-source or you're
-              interested in our paid plans, react out to us on Twitter.
-            </p>
-            <ul className="my-4 space-y-3">
-              <li>
-                <a
-                  href="https://twitter.com/fwuensche"
-                  className="flex items-center p-3 text-base font-bold text-slate-800 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
-                >
-                  <svg
-                    className="inline stroke-slate-700 fill-white w-5 h-5"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Twitter</title>
-                    <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-                  </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">@fwuensche</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/r_chqt"
-                  className="flex items-center p-3 text-base font-bold text-slate-800 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
-                >
-                  <svg
-                    className="inline stroke-slate-700 fill-white w-5 h-5"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Twitter</title>
-                    <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-                  </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">@r_chqt</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <a
+          href="https://github.com/cherrypush/cherrypush.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+        >
+          <FaGithub className="w-5 h-5" />
+        </a>
       </div>
     </footer>
   </>
