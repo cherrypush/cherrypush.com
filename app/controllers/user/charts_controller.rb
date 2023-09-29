99e17f11b84f1251e3643b542a5b3d2b05eeca36
Flavio Wuensche <f.wuensche@gmail.com>
2023-09-29 16:25:05 +0200

    update policies

--- app/controllers/user/charts_controller.rb
+++ app/controllers/user/charts_controller.rb
@@ -3,26 +3,21 @@
 class User::ChartsController < User::ApplicationController
   def create
     dashboard = Dashboard.find(chart_params[:dashboard_id])
-
-    authorize dashboard.project, :read?
-
+    authorize dashboard.project, :write_access?
     chart = dashboard.charts.create!(chart_params)
     params[:chart][:metric_ids].each { |metric_id| chart.chart_metrics.create!(metric_id: metric_id) }
   end
 
   def update
     chart = Chart.find(params[:id])
-    authorize chart.dashboard.project, :read?
+    authorize chart.dashboard.project, :write_access?
     chart.update!(chart_params)
-
-    return if chart.chart_metric_ids == params[:chart][:metric_ids]
-    chart.chart_metrics.destroy_all
-    params[:chart][:metric_ids].each { |metric_id| chart.chart_metrics.create!(metric_id: metric_id) }
+    update_chart_metrics(chart)
   end
 
   def destroy
     chart = Chart.find(params[:id])
-    authorize chart.dashboard.project, :read?
+    authorize chart.dashboard.project, :write_access?
     chart.destroy!
   end
 
@@ -30,5 +25,11 @@ class User::ChartsController < User::ApplicationController
 
   def chart_params
     params.require(:chart).permit(:dashboard_id, :kind)
+  end
+
+  def update_chart_metrics(chart)
+    return if chart.chart_metric_ids == params[:chart][:metric_ids]
+    chart.chart_metrics.destroy_all
+    params[:chart][:metric_ids].each { |metric_id| chart.chart_metrics.create!(metric_id: metric_id) }
   end
 end
