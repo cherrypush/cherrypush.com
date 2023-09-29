ce3ac0af85e5aa2fbf84ddbfcaa1e97e6cb125ea
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-09-29 15:40:19 +0200

    Revert "fix: users should be able to delete metrics from the organizations they belong to"
    This reverts commit 690954f51f4832ee2d4307bce33e3d88ca069631.

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# TODO: remove this policy and purely rely on the OrganizationPolicy
-# This policy is used by user/metrics#index, user/dashboards#show, and user/dashboards#create
-# We should first change the FK from dashboards to organizations, instead of projects, then remove this policy
 class ProjectPolicy < ApplicationPolicy
   def read?
     return true if user.admin?
