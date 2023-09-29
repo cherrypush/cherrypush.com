6a16c339da1306fda30a2bd7666e571adf2a472b
Flavio Wuensche <f.wuensche@gmail.com>
2023-09-29 15:28:23 +0200

    fix: users should be able to delete metrics from the organizations they belong to

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# TODO: remove this policy and purely rely on the OrganizationPolicy
+# This policy is used by user/metrics#index, user/dashboards#show, and user/dashboards#create
+# We should first change the FK from dashboards to organizations, instead of projects, then remove this policy
 class ProjectPolicy < ApplicationPolicy
   def read?
     return true if user.admin?
