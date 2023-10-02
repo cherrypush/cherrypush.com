2cc9318abcaa9f35624ad2ab356e6e67ab304819
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-10-02 20:01:26 +0200

    Revert "paginate notifications"
    This reverts commit c197e9a5eb2ca587132262bc9b5f7db5c41acd79.

--- app/controllers/user/notifications_controller.rb
+++ app/controllers/user/notifications_controller.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
 class User::NotificationsController < User::ApplicationController
-  PER_PAGE = 20
-
   def index
-    render json:
-             current_user
-               .notifications
-               .includes(:item)
-               .page(current_page)
-               .per(PER_PAGE)
-               .order(created_at: :desc)
-               .as_json(include: %i[item])
+    # TODO: this should be paginated
+    render json: current_user.notifications.includes(:item).order(created_at: :desc).as_json(include: %i[item])
   end
 
   def mark_as_seen
@@ -23,11 +15,5 @@ class User::NotificationsController < User::ApplicationController
   def mark_all_as_seen
     current_user.notifications.update_all(seen_at: Time.current)
     head :no_content
-  end
-
-  private
-
-  def current_page
-    params[:page] || 1
   end
 end
