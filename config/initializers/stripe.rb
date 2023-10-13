3309c24953cec072b2d6c9c400766829f54b2cd6
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-10-13 09:13:00 +0200

    always load stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 
-stripe_api_key = ENV.fetch("STRIPE_SECRET_KEY", nil)
-
-Stripe.api_key = stripe_api_key if stripe_api_key.present?
+Stripe.api_key = ENV.fetch("STRIPE_SECRET_KEY", nil)
