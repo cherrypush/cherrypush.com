87e4757be6593eb375cfba3b9d3645e1b32e9650
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-10-13 20:57:44 +0200

    remove deprecated association

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,6 @@ class Project < ApplicationRecord
   has_many :metrics, dependent: :destroy
   has_many :reports, through: :metrics
   has_many :dashboards, dependent: :destroy
-  has_many :authorizations, dependent: :destroy
 
   validates :name, presence: true
   validates :user, presence: true
@@ -15,9 +14,5 @@ class Project < ApplicationRecord
   def owners
     # TODO: this is not necessarily the best way to get the owners, but it works for now
     Report.where(metric: metrics).order(date: :desc).limit(100).map(&:owners).flatten.uniq.sort_by(&:handle)
-  end
-
-  def users
-    User.where(id: authorizations.pluck(:user_id) + [user_id].uniq)
   end
 end
