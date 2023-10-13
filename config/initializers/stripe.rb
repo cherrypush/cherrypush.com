755050f6a6b282d768318cec4e274cb4fdeb55c9
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-10-13 09:10:04 +0200

    do not set stripe api key if env var is missing

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-Stripe.api_key = ENV.fetch("STRIPE_SECRET_KEY", nil)
+stripe_api_key = ENV.fetch("STRIPE_SECRET_KEY", nil)
+
+Stripe.api_key = stripe_api_key if stripe_api_key.present?
