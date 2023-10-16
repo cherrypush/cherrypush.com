75f2b5e670875ebefa5b362e156f1fe6a3e07693
Flavio Wuensche <f.wuensche@gmail.com>
2023-10-16 14:43:08 +0200

    fix tests

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -10,5 +10,6 @@ FactoryBot.define do
     github_handle { Faker::Internet.username }
     provider { "google_oauth2" }
     uid { Faker::Crypto.sha1 }
+    image { Faker::LoremFlickr.image }
   end
 end
