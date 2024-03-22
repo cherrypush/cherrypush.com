3a8e80d78ac61dc97267c9e2ae93a373342601e7
Flavio Wuensche <flavio.wuensche@doctolib.com>
2024-03-22 19:43:01 +0100

    define module

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require 'rails/test_unit/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+module Cherry; end
+
 class Cherry::Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
   config.load_defaults 7.0
